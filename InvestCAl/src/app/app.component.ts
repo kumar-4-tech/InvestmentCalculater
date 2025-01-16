b300f396c9f52abb01ab28dc1a26cc31d1a78bbc
Bhushan Patel <bpatel@konaai.com>
2025-01-16 11:07:35 +0530

    first push for InvestmentCalculater

---
+++ InvestCAl/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+import { HeaderComponent } from './header/header.component';
+import { UserInputComponent } from './user-input/user-input.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [HeaderComponent,UserInputComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'InvestCAL';
+
+  
+}
